c577d29d6744d8200a0872d858cfaf5359d78544
叉叉 <ruihe.lin@live.com>
2025-10-12 10:31:34 +0800

    Update UrlHelper.swift

--- MacCalendar/Utils/UrlHelper.swift
+++ MacCalendar/Utils/UrlHelper.swift
@@ -12,25 +12,13 @@ struct UrlHelper{
     /// - Parameter url: 一个输入的 URL 对象。
     /// - Returns: 一个带有网络协议的 URL 对象。
     static func normalizeURL(from url: URL) -> URL {
-        // 检查 scheme 是否存在并且是网络协议
-        if let scheme = url.scheme, scheme.hasPrefix("http") {
+        if let scheme = url.scheme, scheme.lowercased() == "http" || scheme.lowercased() == "https" {
             return url
         }
         
-        // scheme 不存在或不是 http/https
-        let pathComponent = url.path
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        components?.scheme = "https"
         
-        // 如果路径为空，直接返回原始url，避免创建无效url
-        guard !pathComponent.isEmpty else {
-            return url
-        }
-        
-        // 用 "https://" 和 path 重新构建一个新的 URL
-        if let newURL = URL(string: "https://" + pathComponent) {
-            return newURL
-        }
-        
-        // 失败则返回原始的 URL 作为兜底。
-        return url
+        return components?.url ?? url
     }
 }
