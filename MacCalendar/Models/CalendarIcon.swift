24ecbf7269bee5683f9b6d5ac700a62500b02399
叉叉 <ruihe.lin@live.com>
2026-01-27 21:12:38 +0800

    v1.1.14

--- MacCalendar/Models/CalendarIcon.swift
+++ MacCalendar/Models/CalendarIcon.swift
@@ -5,8 +5,9 @@
 //  Created by ruihelin on 2025/10/6.
 //
 
-import Foundation
+import SwiftUI
 import Combine
+import AppKit
 
 class CalendarIcon: ObservableObject {
     @Published var displayOutput: String = ""
@@ -14,117 +15,139 @@ class CalendarIcon: ObservableObject {
     private var timer: Timer?
     private let dateFormatter = DateFormatter()
     private var cancellables = Set<AnyCancellable>()
-
+    
     init() {
         NotificationCenter.default
             .publisher(for: UserDefaults.didChangeNotification)
             .receive(on: DispatchQueue.main)
             .sink { [weak self] _ in
-                self?.manageTimer()
+                self?.refreshTimer()
             }
             .store(in: &cancellables)
         
-        manageTimer()
+        // 监听系统时间改变（手动修改时间）
+        NotificationCenter.default
+            .publisher(for: NSNotification.Name.NSSystemClockDidChange)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.refreshTimer()
+            }
+            .store(in: &cancellables)
+        
+        // 监听系统唤醒
+        NSWorkspace.shared.notificationCenter
+            .publisher(for: NSWorkspace.didWakeNotification)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.refreshTimer()
+            }
+            .store(in: &cancellables)
+        
+        refreshTimer()
     }
-
+    
     deinit {
         stopTimer()
     }
-
-    private func manageTimer() {
+    
+    private func refreshTimer() {
         stopTimer()
-        
-        let (interval, component) = getUpdateInterval()
-        
-        // 如果是 .icon 模式 (interval == 0)，停止并立即更新
-        guard interval > 0, component != .nanosecond else {
-            updateDisplayOutput()
-            return
-        }
-        
-        // 计算第一次 Tick的时间
-        let now = Date()
-        let calendar = Calendar.current
-        var timeUntilNextTick: TimeInterval
-
-        switch component {
-        case .second:
-            // 计算到下一个整秒还差多少纳秒
-            let nanoseconds = calendar.component(.nanosecond, from: now)
-            timeUntilNextTick = (1_000_000_000.0 - Double(nanoseconds)) / 1_000_000_000.0
-            
-        case .minute:
-            // 计算到下一个整分钟还差多少秒
-            let components = calendar.dateComponents([.second, .nanosecond], from: now)
-            let seconds = Double(components.second ?? 0)
-            let nanoseconds = Double(components.nanosecond ?? 0) / 1_000_000_000.0
-            timeUntilNextTick = 60.0 - (seconds + nanoseconds)
-
-        case .day:
-            // 计算到明天 00:00 还差多少秒
-            let startOfToday = calendar.startOfDay(for: now)
-            let startOfTomorrow = calendar.date(byAdding: .day, value: 1, to: startOfToday)!
-            timeUntilNextTick = startOfTomorrow.timeIntervalSince(now)
-            
-        default:
-            return
-        }
-        
-        // 避免 timeUntilNextTick 几乎为 0 导致重复触发
-        if timeUntilNextTick < 0.001 {
-            timeUntilNextTick += interval
-        }
-        
         updateDisplayOutput()
-
-        self.timer = Timer(timeInterval: timeUntilNextTick, repeats: false) { [weak self] _ in
-            DispatchQueue.main.async {
-                self?.updateDisplayOutput()
-                self?.scheduleRepeatingTimer(interval: interval)
-            }
-        }
-        RunLoop.main.add(timer!, forMode: .common)
+        scheduleNextTick()
     }
-
-    private func scheduleRepeatingTimer(interval: TimeInterval) {
-        stopTimer()
-        
-        self.timer = Timer(timeInterval: interval, repeats: true) { [weak self] _ in
-            self?.updateDisplayOutput()
-        }
-        RunLoop.main.add(timer!, forMode: .common)
-    }
-
+    
     private func stopTimer() {
         timer?.invalidate()
         timer = nil
     }
-
+    
+    private func scheduleNextTick() {
+        let (interval, component) = getUpdateInterval()
+        
+        // 如果是 .icon 模式直接返回
+        guard interval > 0, component != .nanosecond else { return }
+        
+        let now = Date()
+        let calendar = Calendar.current
+        var timeUntilNextTick: TimeInterval = 0
+        
+        switch component {
+        case .second:
+            // 对齐到下一秒
+            let nanoseconds = calendar.component(.nanosecond, from: now)
+            timeUntilNextTick = (1_000_000_000.0 - Double(nanoseconds)) / 1_000_000_000.0
+            
+        case .minute:
+            // 对齐到下一分
+            let components = calendar.dateComponents([.second, .nanosecond], from: now)
+            let seconds = Double(components.second ?? 0)
+            let nanoseconds = Double(components.nanosecond ?? 0) / 1_000_000_000.0
+            timeUntilNextTick = 60.0 - (seconds + nanoseconds)
+            
+        case .day:
+            // 对齐下一天
+            let startOfToday = calendar.startOfDay(for: now)
+            if let startOfTomorrow = calendar.date(byAdding: .day, value: 1, to: startOfToday) {
+                timeUntilNextTick = startOfTomorrow.timeIntervalSince(now)
+            } else {
+                timeUntilNextTick = 86400
+            }
+            
+        default:
+            // 为了防止死锁，默认按秒更新
+            timeUntilNextTick = 1.0
+        }
+        
+        // 强制增加一点延迟，防止 Timer 狂转
+        if timeUntilNextTick < 0.05 {
+            timeUntilNextTick += 0.1
+        }
+        
+        self.timer = Timer(timeInterval: timeUntilNextTick, repeats: false) { [weak self] _ in
+            DispatchQueue.main.async {
+                self?.updateDisplayOutput()
+                self?.scheduleNextTick()
+            }
+        }
+        
+        // 加入 RunLoop，防止菜单滚动时计时器暂停
+        if let timer = self.timer {
+            RunLoop.main.add(timer, forMode: .common)
+        }
+    }
+    
     private func getUpdateInterval() -> (interval: TimeInterval, component: Calendar.Component) {
         switch SettingsManager.displayMode {
         case .icon:
-            return (0, .nanosecond)
+            return (0, .nanosecond) // 不更新
             
         case .time:
-            return (1.0, .second)
+            return (1.0, .second) // 秒级更新
             
         case .date:
-            return (60 * 60 * 24, .day)
+            return (86400, .day) // 天级更新
             
         case .custom:
             let format = SettingsManager.customFormatString
-            if format.contains("s") {
+            
+            // 如果包含 s S，按秒更新
+            if format.contains("s") || format.contains("S") {
                 return (1.0, .second)
-            } else if format.contains("m") || format.contains("h") || format.contains("H") || format.contains("a") {
+            }
+            // 如果包含 m H h，按分钟更新
+            else if format.contains("m") || format.contains("h") || format.contains("H") || format.contains("a") {
                 return (60.0, .minute)
-            } else {
-                return (60 * 60 * 24, .day)
+            }
+            // 按天更新
+            else {
+                return (86400, .day)
             }
         }
     }
-
+    
     private func updateDisplayOutput() {
-        dateFormatter.locale = Locale.current
+        dateFormatter.calendar = Calendar.autoupdatingCurrent
+        dateFormatter.locale = Locale.autoupdatingCurrent
         
         switch SettingsManager.displayMode {
         case .icon:
@@ -137,13 +160,15 @@ class CalendarIcon: ObservableObject {
             displayOutput = dateFormatter.string(from: Date())
         case .custom:
             dateFormatter.dateFormat = SettingsManager.customFormatString
+            
             if SettingsManager.customFormatString.contains("w") {
-                            var calendar = Calendar(identifier: .iso8601)
-                            // ISO 8601 标准：周一为第一天，第一周至少4天
-                            calendar.firstWeekday = 2 // 2 代表周一
-                            calendar.minimumDaysInFirstWeek = 4
-                            dateFormatter.calendar = calendar
-                        }
+                var calendar = Calendar(identifier: .iso8601)
+                // ISO 8601 标准：周一为第一天，第一周至少4天
+                calendar.firstWeekday = 2 // 2 代表周一
+                calendar.minimumDaysInFirstWeek = 4
+                dateFormatter.calendar = calendar
+            }
+            
             displayOutput = dateFormatter.string(from: Date())
         }
     }
